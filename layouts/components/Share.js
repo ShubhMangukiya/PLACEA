97e04f06a6e97663605562f633ea5e1722c45170
ShubhMangukiya <shubh.mangukiya01@gmail.com>
2023-08-16 19:35:02 +0530

    share change

--- layouts/components/Share.js
+++ layouts/components/Share.js
@@ -1,9 +1,4 @@
 import config from "@config/config.json";
-import { plainify } from "@lib/utils/textConverter";
-import Footer from "@partials/Footer";
-import Header from "@partials/Header";
-import Head from "next/head";
-import { useRouter } from "next/router";
 
 import {
   IoLogoFacebook,
@@ -14,15 +9,13 @@ import {
 
 const Share = ({ title, description, slug, className }) => {
   // destructuring items from config object
-  const { base_url } = 'https://www.placea.in'
-  const router = useRouter();
 
   return (
     <ul className={`${className}`}>
       <li className="inline-block">
         <a
           aria-label="facebook share button"
-          href={`https://facebook.com/sharer/sharer.php?u=${base_url}/${slug}`}
+          href={`https://facebook.com/sharer/sharer.php?u=https://www.placea.in/${slug}`}
           target="_blank"
           rel="noreferrer noopener"
           button="true"
@@ -33,7 +26,7 @@ const Share = ({ title, description, slug, className }) => {
       <li className="inline-block">
         <a
           aria-label="twitter share button"
-          href={`https://twitter.com/intent/tweet/?text=${title}&amp;url=${base_url}/${slug}`}
+          href={`hhttps://twitter.com/intent/tweet/?amp;url=https://www.placea.in/${slug}`}
           target="_blank"
           rel="noreferrer noopener"
           button="true"
@@ -44,7 +37,7 @@ const Share = ({ title, description, slug, className }) => {
       <li className="inline-block">
         <a
           aria-label="linkedin share button"
-          href={`https://www.linkedin.com/shareArticle?mini=true&url=${base_url}/${slug}&title=${title}&summary=${description}&source=${base_url}`}
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=https://www.placea.in/${slug}&title=${title}&summary=${description}&source=${base_url}`}
           target="_blank"
           rel="noreferrer noopener"
         >
@@ -54,7 +47,7 @@ const Share = ({ title, description, slug, className }) => {
       <li className="inline-block">
         <a
           aria-label="pinterest share button"
-          href={`https://pinterest.com/pin/create/button/?url=${base_url}/${slug}&media=&description=${description}`}
+          href={`https://pinterest.com/pin/create/button/?url=https://www.placea.in/${slug}&media=&description=${description}`}
           target="_blank"
           rel="noreferrer noopener"
           button="true"
