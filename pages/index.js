6b77a71c5382370ec5d4c47188c9f8396f7a1e52
ShubhMangukiya <shubh.mangukiya01@gmail.com>
2023-08-17 11:36:30 +0530

    ga

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,22 @@
 import BlogPagination, { getStaticProps } from "./page/[slug]";
 export { getStaticProps };
 import Script from 'next/script'
+import Script from 'next/script'
+
+function Home() {
+  return (
+    <div className="container">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-RWW57SD920" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-RWW57SD920');
+        `}
+      </Script>
+    </div>
+  )
+}
 export default BlogPagination;
